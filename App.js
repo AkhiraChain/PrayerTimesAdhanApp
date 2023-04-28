88304ecdbf412d18f81d6fc5bb338968df1d3a6f
Mohammed M <mohamgoham@gmail.com>
2023-04-28 02:04:02 -0400

    add react dimension hooks

--- App.js
+++ App.js
@@ -1,6 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, Image, SafeAreaView, TouchableOpacity, Alert, Button } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  Image, 
+  SafeAreaView, 
+  TouchableOpacity, 
+  Alert, 
+  Button,
+  View,
+ } from 'react-native';
 
+ import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
 // View --> UIView
 export default function App() {
 
@@ -31,6 +41,42 @@ source={require('./assets/ll.png')}/>
         ])
         }
         />
+          <Button  
+        color="black"
+        title="Dhurh Time" 
+        onPress={() => Alert.alert("Adhan API", "Akhira Labs team is working hard to bring the audit & test the PrayerTimes App, free from Ads!, Spyware! and Data harvesting!!", [
+          { text: "Sign up for Beta", onPress: () => console.log("Beta Pressed")},
+          { text: "Cancel", onPress: () => console.log("Cancel Pressed")},
+        ])
+        }
+        />
+          <Button  
+        color="black"
+        title="Asr Time" 
+        onPress={() => Alert.alert("Adhan API", "Akhira Labs team is working hard to bring the audit & test the PrayerTimes App, free from Ads!, Spyware! and Data harvesting!!", [
+          { text: "Sign up for Beta", onPress: () => console.log("Beta Pressed")},
+          { text: "Cancel", onPress: () => console.log("Cancel Pressed")},
+        ])
+        }
+        />
+          <Button  
+        color="black"
+        title="Maghrib Time" 
+        onPress={() => Alert.alert("Adhan API", "Akhira Labs team is working hard to bring the audit & test the PrayerTimes App, free from Ads!, Spyware! and Data harvesting!!", [
+          { text: "Sign up for Beta", onPress: () => console.log("Beta Pressed")},
+          { text: "Cancel", onPress: () => console.log("Cancel Pressed")},
+        ])
+        }
+        />
+          <Button  
+        color="black"
+        title="Isha Time" 
+        onPress={() => Alert.alert("Adhan API", "Akhira Labs team is working hard to bring the audit & test the PrayerTimes App, free from Ads!, Spyware! and Data harvesting!!", [
+          { text: "Sign up for Beta", onPress: () => console.log("Beta Pressed")},
+          { text: "Cancel", onPress: () => console.log("Cancel Pressed")},
+        ])
+        }
+        />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -42,6 +88,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'magenta',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Platform.OS === "android" ? 20 : 0,
   },
   txt: {
     color: 'cyan',
